10ff71103ee4f66fa9e2238cb638b5c5ee941f79
koheleth <coaleth@gmail.com>
2023-07-27 08:32:46 -0300

    atualizacao recente

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ api = Api(app)
 
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
-
+#
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
